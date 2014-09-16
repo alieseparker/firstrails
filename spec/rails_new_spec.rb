45ce2cbcfaddc28b89e90ab173206f30f216d9a5
Forrest Parker <forresta.parker@gmail.com>
2014-09-16 09:58:46 -0700

    final test pass

--- spec/rails_new_spec.rb
+++ spec/rails_new_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe "My Rails App welcome page" do
   it "shows the welcome message" do
     visit "http://localhost:3000"
-    page.text.must_include "Welcome Aboard"
+    page.text.must_include "Welcome aboard"
   end
 end
